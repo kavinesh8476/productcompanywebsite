a350930c8509f6b93f4d5c7cfaab8c8d219c3a78
kavinesh8476 <kavineshtamil18@gmail.com>
2023-01-19 09:37:06 +0000

    updated

--- companywebsite/companywebsite/urls.py
+++ companywebsite/companywebsite/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from productcompany import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.company,name="company"),
+    path("products.html",views.products,name='products'),
+    path("home.html", views.company),
+    path("people.html",views.people),
+    path("contact.html",views.contact),
 ]
